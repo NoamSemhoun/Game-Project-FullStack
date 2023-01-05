fbe334aaedce1f0aefbca3a89a0e106df686b4d2
Yoel Obadia <yobadia@g.jct.ac.il>
2023-01-05 17:00:25 +0200

    game1 level2 finished + update main page

--- Main_page/script.js
+++ Main_page/script.js
@@ -1,14 +1,27 @@
-function storage(event) {
+function storage() {
     var userName = document.getElementById("Username").value;
     var pwd = document.getElementById("Password").value;
+    var email = document.getElementById("Email").value;
+    var age = document.getElementById("Age").value;
 
     localStorage.setItem('Username', userName);
     localStorage.setItem('Password', pwd);
+    localStorage.setItem('Email', email);
+    localStorage.setItem('Age', age);
 
-    document.getElementById("entryForm").reset(); 
+    document.getElementById("myForm").reset(); // clear the form when changing page
+    
 }
 
-function check(event) {
+function check() {
+    let a = 0; // counter for number of tries to authenticate
+    while(!check1(i) && a < 6) {
+        check1(i);
+        a+=i;
+    }
+}
+
+function check1(i) {
     var tries = 0;
     var userName = document.getElementById("Username").value;
     var pwd = document.getElementById("Password").value;
@@ -16,12 +29,25 @@ function check(event) {
     if(userName === localStorage.key("Username") && pwd === localStorage.key("Password")){
         tries += 1;
         alert("Authentication success!");
+        return true;
+    } else {
+        tries += 1;
+        alert("Failed to authenticate!");
+        return false;
     }
 } 
 
-function setCookie() {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    cookievalue = escape(document.myform.customer.value) + ";";
-    document.cookie = "name= " + cookieValue + "expires= " + d.toUTCString();
+var today = new Date();
+var expiry = new Date(today.getTime() + 30 * 24 * 3600 * 1000); // plus 30 days
+
+function setCookie(name, value) {
+    document.cookie=name + "=" + escape(value) + "; path=/; expires=" + expiry.toGMTString();
+}
+
+function storeValues(form)  {
+    setCookie("Username", form.field1.value);
+    setCookie("Password", form.field2.value);
+
+    document.getElementById("entryForm").reset(); // clear the form when changing page
+    return true;
 }
