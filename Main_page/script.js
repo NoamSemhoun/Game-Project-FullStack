c5bb2b1f59874c8d67bf51ad728d96085bf2d184
Yoel Obadia <yobadia@g.jct.ac.il>
2023-01-05 15:38:26 +0200

    update main page

--- Main_page/script.js
+++ Main_page/script.js
@@ -6,5 +6,22 @@ function storage(event) {
     localStorage.setItem('Password', pwd);
 
     document.getElementById("entryForm").reset(); 
-    // gg
+}
+
+function check(event) {
+    var tries = 0;
+    var userName = document.getElementById("Username").value;
+    var pwd = document.getElementById("Password").value;
+
+    if(userName === localStorage.key("Username") && pwd === localStorage.key("Password")){
+        tries += 1;
+        alert("Authentication success!");
+    }
+} 
+
+function setCookie() {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    cookievalue = escape(document.myform.customer.value) + ";";
+    document.cookie = "name= " + cookieValue + "expires= " + d.toUTCString();
 }
