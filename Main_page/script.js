87cfdcb664a267c9418a4ec5cf00a1cf2abc7798
Yoel Obadia <yobadia@g.jct.ac.il>
2023-01-09 14:08:11 +0200

    essais form functions

--- Main_page/script.js
+++ Main_page/script.js
@@ -1,49 +1,56 @@
-function create() {
-    var userName = document.getElementById("Username").value;
-    var pwd = document.getElementById("Password").value;
-    var confirmation = document.getElementById("PasswordConfirmation").value;
-
-    if(confirmation != pwd){
-        alert("Sign up failed! Try again!")
-        pwd.reset();
-        confirmation.reset();
-        return;
-    } else {
-        localStorage.setItem('Username', userName);
-        localStorage.setItem('Password', pwd);
-
-        document.getElementById("registerForm").reset(); // clear the form when changing page
-        alert("Sign up success!");
-        
-        document.location.href="..\\ChoiceGames_Page\\choiceGames.html";
-        return;
-    }
-}
 var turn = 0;
 
 function check() {
     var username = document.getElementById("Username").value;
     var password = document.getElementById("Password").value;
-    
+
 
     var storedUsername = localStorage.getItem("Username").value;
     var storedPassword = localStorage.getItem("Password").value;
 
-    if(username === storedUsername && password === storedPassword) {
+    if (username === storedUsername && password === storedPassword) {
         alert("Login success!")
         username.reset();
         password.reset();
-        document.location.href="..\\ChoiceGames_Page\\choiceGames.html";
+        document.location.href = "..\\ChoiceGames_Page\\choiceGames.html";
         return;
     } else {
         alert("Failed to login");
         turn += 1;
     }
 
-    if(turn == 5) {
+    if (turn == 5) {
         localStorage.removeItem("Username");
         localStorage.removeItem("Password");
         alert("Too much fails! You need to register!")
         return;
+    }
+}
+
+//Event for the sign up form
+function a(e){
+    var userName = document.getElementById("Username");
+    var pwd = document.getElementById("Password");
+    var confirmation = document.getElementById("PasswordConfirmation");
+
+    if(confirmation.value == pwd.value) {
+        localStorage.setItem('Username', userName.value);
+        localStorage.setItem('Password', pwd.value);
+
+        userName.value.reset();
+        pwd.value.reset();
+        confirmation.value.reset();
+        //document.getElementById("registerForm").reset(); 
+        alert("Sign up success!");
+
+        document.location.href = "../ChoiceGames_Page/choiceGames.html";
+        return;
+        
+    } else {
+        alert("Sign up failed! Try again!")
+        pwd.reset();
+        confirmation.reset();
+        e.preventDefault();
+        return false;
     } 
 }
